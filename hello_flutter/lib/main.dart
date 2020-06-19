f3cd86bce73fc018132044698c4b169ff1ec2ce9
Mradul Jain <er.mradulrathore@gmail.com>
2020-06-19 12:30:56 +0530

    Flutter

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+
+void main() {
+  runApp(new HelloFlutterApp());
+}
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App Bar"),
+        ),
+        body: Home(),
+      ),
+    );
+  }
+
+}
